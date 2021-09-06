dd6a0273be22bf7983df347805f48870ad2688d0
Raphael V <veauville.raphael@gmail.com>
2021-09-06 14:48:24 +0200

    Update rules.ts

--- src/commands/Admin/rules.ts
+++ src/commands/Admin/rules.ts
@@ -51,7 +51,7 @@ Server Invite: https://discord.gg/qgd85nEf5a
 `
       )
       .setImage(
-        "https://cdn.discordapp.com/attachments/877217860408209449/879781364742893598/20210824_191118_0000__01.png"
+        "https://media.discordapp.net/attachments/881988260925153322/882027519753224244/sheweny_baniere.png"
       )
       .setTimestamp()
       .setFooter("Sheweny discord server rules");
