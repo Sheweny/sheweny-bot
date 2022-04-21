ef6a5f724e332e9b079ad2c79d7140d69978b598
Veauville Raphael <veauville.raphael@gmail.com>
2022-04-21 20:54:06 +0200

    Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,10 +4,6 @@ module.exports = {
       name: "sheweny-bot",
       cwd: "./dist",
       script: "node index.js",
-      env: {
-        NODE_ENV: "production",
-        BOT_TOKEN: "lp",
-      },
     },
   ],
 };
