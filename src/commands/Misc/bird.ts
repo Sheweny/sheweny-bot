3d9774360579c8f0b7f5f35b7f5fa76e0fe92324
p-bzh <ronan.questier@mailfence.com>
2022-07-11 01:56:15 +0300

    end of usefull commands

---
+++ src/commands/Misc/bird.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { Command } from "sheweny";
+import { CommandInteraction, MessageEmbed } from "discord.js";
+import type { ShewenyClient } from "sheweny";
+
+export class Bird extends Command {
+  constructor(client: ShewenyClient) {
+    super(client, {
+      name: "bird",
+      type: "SLASH_COMMAND",
+      category: "Misc",
+      description: "Return random bird picture.",
+      cooldown: 10,
+    });
+  }
+
+  async execute(interaction: CommandInteraction) {
+    const request = await axios.get("http://shibe.online/api/birds");
+    const { data } = request;
+    const embed = new MessageEmbed().setImage(data[0]);
+    interaction.reply({ embeds: [embed] });
+  }
+}
